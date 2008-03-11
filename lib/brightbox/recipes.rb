f4a22b7a963230980a3c8989ce3b25fda1f2cbaf
brightbox <brightbox@f14820b4-53c2-4c7b-be4b-41e81b371297>
2008-03-11 19:29:08 +0000

    rejig read_db_config to use rails_env ensure create_database returns any failures to capistrano added deploy:migrations task to deploy and run migrations added load_schema task to run the db:schema:load task sleep after monit:reload rejig monit related mongrel tasks into monit:mongrel namespace add apache:restart task add monit:restart task
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brightbox/brightbox-gem/trunk@36 f14820b4-53c2-4c7b-be4b-41e81b371297

--- lib/brightbox/recipes.rb
+++ lib/brightbox/recipes.rb
@@ -1,3 +1,4 @@
+require 'mongrel_cluster/recipes'
 Capistrano.configuration(:must_exist).load do
 
 namespace :apache do
@@ -12,40 +13,52 @@ namespace :apache do
     sudo "/usr/bin/brightbox-apache -n #{application} -d #{domain} -a #{domain_aliases} -w #{current_path}/public -h #{mongrel_host} -p #{mongrel_port} -s #{mongrel_servers}"
     sudo "/usr/sbin/apache2ctl -t"
   end
-
-end
-
-namespace :mongrel do
-  desc "Configure monit to handle the mongrel servers for this app"
-  task :configure_cluster, :roles => :app do
-    sudo "/usr/bin/brightbox-monit -n #{application} -r #{current_path} -p #{mongrel_port} -s #{mongrel_servers} -h #{mongrel_host}"
+  
+  desc "Restart Apache on your Brightbox web servers"
+  task :restart, :roles => :web do
+    sudo "/usr/sbin/apache2ctl -t"
+    sudo "/etc/init.d/apache2 restart"
   end
 
-  desc "Restart the mongrel servers using monit"
-  task :restart_cluster, :roles => :app do
-    sudo "/usr/sbin/monit -g #{application} restart all"
-  end
-
-  desc "Start the mongrel servers using monit"
-  task :start_cluster, :roles => :app do
-    sudo "/usr/sbin/monit -g #{application} start all"
-  end
-
-  desc "Stop the mongrel servers using monit"
-  task :stop_cluster, :roles => :app do
-    sudo "/usr/sbin/monit -g #{application} stop all"
-  end
 end
 
 namespace :monit do
+  desc "Configure monit to handle the mongrel servers for this app"
+  task :configure, :roles => :app do
+    sudo "/usr/bin/brightbox-monit -n #{application} -r #{current_path} -p #{mongrel_port} -s #{mongrel_servers} -h #{mongrel_host}"
+  end
+  
   desc "Display the monit status summary"
   task :status, :roles => :app do
-    sudo "/usr/sbin/monit -g #{application} status"
+    sudo "/usr/sbin/monit status"
   end
 
   desc "Reload the monit daemon"
   task :reload, :roles => :app do
     sudo "/usr/sbin/monit reload"
+    sleep 5
+  end
+  
+  desc "Restart the monit daemon"
+  task :restart, :roles => :app do
+    sudo "/etc/init.d/monit restart"
+  end
+
+  namespace :mongrel
+    desc "Restart the mongrel servers using monit"
+    task :restart_cluster, :roles => :app do
+      sudo "/usr/sbin/monit -g #{application} restart all"
+    end
+
+    desc "Start the mongrel servers using monit"
+    task :start_cluster, :roles => :app do
+      sudo "/usr/sbin/monit -g #{application} start all"
+    end
+
+    desc "Stop the mongrel servers using monit"
+    task :stop_cluster, :roles => :app do
+      sudo "/usr/sbin/monit -g #{application} stop all"
+    end
   end
 end
 
@@ -56,11 +69,13 @@ deploy.task :cold do
     deploy.symlink
   end
 
-  mysql.create_database
+  mysql.create_database # Fails and stops here if db already exists
+  load_schema # WARNING: This wipes any existing tables
   migrate
   mongrel.configure_cluster
+  monit.configure
   monit.reload
-  mongrel.start_cluster
+  monit.mongrel.start_cluster
   apache.setup
   apache.reload
 end
@@ -70,12 +85,29 @@ deploy.task :default do
   transaction do
     deploy.update_code
     web.disable
-    mongrel.stop_cluster
     symlink
-    mongrel.start_cluster
+    monit.mongrel.restart_cluster
   end
 
   web.enable
+end
+
+desc "Deploy the app to your Brightbox servers and run any outstanding migrations"
+deploy.task :migrations do
+  transaction do
+    deploy.update_code
+    web.disable
+    symlink
+    migrate
+    monit.mongrel.restart_cluster
+  end
+
+  web.enable
+end
+
+desc "Load the rails db schema on the primary db server"
+task :load_schema, :roles => :db, :primary => true do
+  run "cd #{current_path} && #{rake} RAILS_ENV=#{(rails_env||"production").to_s} db:schema:load"
 end
 
 namespace :mysql do
@@ -83,7 +115,7 @@ namespace :mysql do
   task :create_database, :roles => :db, :primary => true do
     read_db_config
     if db_adapter == 'mysql'
-      run "mysql -h #{db_host} --user=#{db_user} -p --execute=\"CREATE DATABASE #{db_name}\" || true" do |channel, stream, data|
+      run "mysql -h #{db_host} --user=#{db_user} -p --execute=\"CREATE DATABASE #{db_name}\"" do |channel, stream, data|
         if data =~ /^Enter password:/
           logger.info data, "[database on #{channel[:host]} asked for password]"
           channel.send_data "#{db_password}\n" 
@@ -95,11 +127,17 @@ end
   
 def read_db_config
     db_config = YAML.load_file('config/database.yml')
-    set :db_adapter, db_config["production"]["adapter"]
-    set :db_user, db_config["production"]["username"]
-    set :db_password, db_config["production"]["password"] 
-    set :db_name, db_config["production"]["database"]
-    set :db_host, db_config["production"]["host"]
+    set :db_adapter, db_config[(rails_env||"production").to_s]["adapter"]
+    set :db_user, db_config[(rails_env||"production").to_s]["username"]
+    set :db_password, db_config[(rails_env||"production").to_s]["password"] 
+    set :db_name, db_config[(rails_env||"production").to_s]["database"]
+    set :db_host, db_config[(rails_env||"production").to_s]["host"]
+    if db_host !~ /^sqlreadwrite/
+      logger.important "WARNING: Database host is not sqlreadwrite as per the Brightbox requirements"
+    end
+    if db_name !~ /^#{db_user}\_/
+      logger.important "WARNING: Database name is not prefixed with MySQL username as per the Brightbox requirements"
+    end
 end
 
 end
