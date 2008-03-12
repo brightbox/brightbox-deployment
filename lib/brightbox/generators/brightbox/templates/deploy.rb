c78b08dc0d8daaa4b2ccf3fd2184874ee586e839
brightbox <brightbox@f14820b4-53c2-4c7b-be4b-41e81b371297>
2008-03-12 12:22:52 +0000

    Add logrotation configs Update ssh options example Rejig subversion config example add domain_alises setting
    git-svn-id: svn+ssh://rubyforge.org/var/svn/brightbox/brightbox-gem/trunk@40 f14820b4-53c2-4c7b-be4b-41e81b371297

--- lib/brightbox/generators/brightbox/templates/deploy.rb
+++ lib/brightbox/generators/brightbox/templates/deploy.rb
@@ -1,20 +1,28 @@
 require 'brightbox/recipes'
+## Created with the brightbox command on <%= Time.now %>
 
 # The name of your application.  Used for deployment directory and filenames
-# and apache configs
+# and Apache configs. Should be unique on the Brightbox
 set :application, "<%= singular_name %>"
-
-# Target directory for the application on the web and app servers.
-set :deploy_to, "/home/rails/#{application}"
-
-# Primary domain name of your application.  Used in the Apache configs
-set :domain, "<%= domain_name %>"
 
 # Login user for ssh on your Brightbox server
 set :user, "rails"
 
-# URL of your source repository - there i
-set :repository, "svn+ssh://rails@<%= server %>/home/rails/subversion/<%= singular_name %>/trunk"
+# Target directory for the application on the web and app servers.
+set :deploy_to, "/home/rails/#{application}"
+
+# Primary domain name of your application. Used in the Apache configs
+set :domain, "<%= domain_name %>"
+# Comma separated list of additional domains for Apache
+set :domain_aliases, ""
+
+# URL of your source repository. This is the default one that comes on 
+# every Brightbox, you can use your own (we'll let you :)
+set :repository, "svn+ssh://<%= server %>/home/rails/subversion/<%= singular_name %>/trunk"
+
+# set :scm, :subversion
+# set :scm_username, "rails"
+# set :scm_password, "mysecret"
 
 role :web, "<%= server %>"
 role :app, "<%= server %>"
@@ -27,15 +35,20 @@ set :mongrel_port, 9200
 set :mongrel_servers, 2
 set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
 
+## Logrotation
+# Where the logs are stored
+set :log_dir, "#{deploy_to}/shared/log"
+# The size at which to rotate a log
+set :log_max_size, "100M"
+# How many old compressed logs to keep
+set :log_keep, "10"
+
 # set :scm, :darcs               # defaults to :subversion
 # set :svn, "/path/to/svn"       # defaults to searching the PATH
 # set :darcs, "/path/to/darcs"   # defaults to searching the PATH
 # set :cvs, "/path/to/cvs"       # defaults to searching the PATH
 # set :gateway, "gate.host.com"  # default to no gateway
 
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
+# set :ssh_options, { :keys => %w(/path/to/my/key /path/to/another/key) }
+
 
