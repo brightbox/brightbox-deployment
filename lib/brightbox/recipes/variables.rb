44f4b1f3f82b5997b86d98b2e6d3b075c7640ecf
Neil Wilson <neil@brightbox.co.uk>
2008-07-30 13:34:36 +0100

    Add in new variables to handle monit parameters. Alter mongrel_host to handle mongrels on separate servers from web.

--- lib/brightbox/recipes/variables.rb
+++ lib/brightbox/recipes/variables.rb
@@ -22,10 +22,13 @@ _cset :mongrel_pid_file, "tmp/pids/mongrel.pid"  #Stores Process ID for Mongrels
 _cset :log_max_size, "100M"                 #Size at which to rotate log
 _cset :log_keep, "10"                       #Keep this many compressed logs
 _cset(:log_dir) {File.join(current_path, 'log')}
-_cset :mongrel_host, "localhost"
+_cset :mongrel_host, :local
 _cset :mongrel_port, 9200
 _cset :mongrel_servers, 2
 _cset(:mongrel_config_file) {File.join(deploy_to, "#{application}_mongrel_config.yml")}
+_cset :mongrel_check_url, "http://localhost"
+_cset :mongrel_max_memory, 110
+_cset :mongrel_max_cpu, 80
 _cset(:domain) { abort "You need to set the :domain variable, e.g set :domain 'www.example.com'" }
 _cset :domain_aliases, nil
 _cset :user, "rails"
