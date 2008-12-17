a68050e0be3a7480caa33605006de7ae8798d6b9
David Smalley <david@davidsmalley.com>
2008-12-17 23:39:20 +0000

    Added licenses to new files. Also, include status in the over-ridden commands

--- lib/brightbox/recipes/deploy/passenger.rb
+++ lib/brightbox/recipes/deploy/passenger.rb
@@ -1,8 +1,28 @@
+#    Brightbox - Easy Ruby Web Application Deployment
+#    Copyright (C) 2008, David Smalley, Brightbox Systems
+#
+#    This file is part of the Brightbox deployment system
+#
+#    Brightbox gem is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License,
+#    or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General
+#    Public License along with this program.  If not, see
+#    <http://www.gnu.org/licenses/>.
+#
+
 namespace :deploy do
   
   #Override start and stop, they are useless for passenger
-  %w(start stop).each do |event|
-    desc "Dummy command to #{event} your application. This is not needed when using passenger."
+  %w(start stop status).each do |event|
+    desc "Dummy command to #{event} your application, not used by Passenger."
     task event, :roles => :app, :except => {:no_release => true } do
     end
   end
