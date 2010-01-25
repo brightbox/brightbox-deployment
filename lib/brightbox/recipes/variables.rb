96baee2a3922dd93eb472119adec1a42942653ec
Caius Durling <caius@brightbox.co.uk>
2010-01-25 14:39:00 +0000

    Fixes #1253 - Allow disabling of webserver config generation
    Need to add "set :disable_webserver_config, false" to your deploy.rb to disable it.

--- lib/brightbox/recipes/variables.rb
+++ lib/brightbox/recipes/variables.rb
@@ -44,6 +44,7 @@ _cset :use_sudo, false
 _cset :ssl_certificate, nil
 _cset :ssl_intermediate, nil
 _cset :ssl_key, nil
+_cset :generate_webserver_config, true
 _cset :rails_env, "production"
 ssh_options[:forward_agent] = true
 
